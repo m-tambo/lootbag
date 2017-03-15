03fc3a30f12119c23b3e30febb9448ea5c3c9e8d
Michael Tambornino <michael.tambornino@gmail.com>
2017-03-15 16:16:17 -0500

    basic function structure

--- lootbag.js
+++ lootbag.js
@@ -1,3 +1,47 @@
 #!/usr/bin/env node
 
 'use strict'
+
+const { readFile, writeFile } = require('fs')
+const { argv: [,, ...args] } = process
+const lootFile = './loot.json'
+
+// const writeData = fs.writeFile()
+// const readData = fs.readFile()
+
+module.exports.getToysByKid = (kidName) => { // this function returns one thing only: a PROMISE
+  return new Promise( (resolve, reject) => {
+    readFile(lootFile, (err, data) => {
+      if (err) return reject(err)
+      let toys = JSON.parse(data).loot[kidName].toys // turn the jason data into a pojo (plain old js object)
+      resolve(toys)
+    })
+  })
+}
+
+module.exports.addToy = (toy, kidName) => {
+  return new Promise( (resolve, reject) => {
+    readFile(lootFile, (err, data) => {
+      if (err) {
+        reject(err)
+      }
+      let toyData = JSON.parse(data).loot[kidName].toys
+
+    })
+    .then ()
+      writeFile(lootFile, toyData, )
+
+  })
+}
+
+module.exports.removeToy = () => {
+  return 'Toy removed'
+}
+
+module.exports.getListOfKids = () => {
+  return []
+}
+
+module.exports.makeKidHappy = () => {
+  return true
+}
